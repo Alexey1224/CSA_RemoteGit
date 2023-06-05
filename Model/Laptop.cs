086f1138207448c1d58ce20111489306a0024d7d
Алексей Алистратов <lexa2721@mail.ru>
2023-06-05 21:41:22 +0700

    [200~I added the id property to the class of my model and

---
+++ Model/Laptop.cs
@@ -0,0 +1,31 @@
+﻿using CSA.Model;
+using System;
+using System.Xml.Linq;
+
+namespace CSA.Model
+{
+    public class Laptop
+    {
+        public Guid Id { get; set; } = Guid.Empty;
+        public string model { get; set; }
+        public string firm { get; set; }
+        public double price { get; set; }
+        public double inch { get; set; }
+
+        public BaseModelValidationResult Validate()
+        {
+            var validationResult = new BaseModelValidationResult();
+
+            if (string.IsNullOrWhiteSpace(model)) validationResult.Append($"Model cannot be empty");
+            if (string.IsNullOrWhiteSpace(firm)) validationResult.Append($"Firm cannot be empty");
+            if (!(price < 0)) validationResult.Append($"Price {price} is out of range");
+            if (!(inch < 0)) validationResult.Append($"Inch {inch} is out of range");
+            return validationResult;
+        }
+
+        public override string ToString()
+        {
+            return $"{model} {firm} from {price}-{inch}";
+        }
+    }
+}
